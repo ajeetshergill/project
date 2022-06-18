7a780adfbf36614ffa2ae806b90d81e5dcc43a40
Ajeet Gill <ajeetshergill@gmail.com>
2022-06-18 15:06:21 +0530

    Added pop-up menu

---
+++ dapp.deadd524.js
@@ -0,0 +1,2 @@
+function openAdditionalMenu(){const d=document.querySelector(".additionalMenu");d.classList.contains("hidden")?(d.classList.add("additionalMenuStyle"),d.classList.remove("hidden")):(d.classList.add("hidden"),d.classList.remove("additionalMenuStyle"))}
+//# sourceMappingURL=dapp.deadd524.js.map
