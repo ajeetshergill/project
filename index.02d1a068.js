e215e6bcfaa8d5f2fb80eab8b1a970d87118b8b9
Ajeet Gill <ajeetshergill@gmail.com>
2022-06-29 10:10:15 +0530

    Redesigned dapp page

---
+++ index.02d1a068.js
@@ -0,0 +1,2 @@
+function openAdditionalMenu(){const d=document.querySelector(".additionalMenu");d.classList.contains("hidden")?(d.classList.add("additionalMenuStyle"),d.classList.remove("hidden")):(d.classList.add("hidden"),d.classList.remove("additionalMenuStyle"))}
+//# sourceMappingURL=index.02d1a068.js.map
