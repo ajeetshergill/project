7b7acbce44b6fd1c79cb4ce731e7dbca12548a63
Ajeet Gill <ajeetshergill@gmail.com>
2022-07-06 00:36:07 +0530

    Temp page
     - added web3 to their presale page design

--- index.02d1a068.js
+++
@@ -1,2 +0,0 @@
-function openAdditionalMenu(){const d=document.querySelector(".additionalMenu");d.classList.contains("hidden")?(d.classList.add("additionalMenuStyle"),d.classList.remove("hidden")):(d.classList.add("hidden"),d.classList.remove("additionalMenuStyle"))}
-//# sourceMappingURL=index.02d1a068.js.map
---
+++ index.b09637e9.js
@@ -0,0 +1,2 @@
+function showErrMsg(s,r,e){s&&r?(r.innerHTML=e,s.classList.remove("opacity-0"),s.classList.add("opacity-100"),setTimeout(hideErrMsg,1e3,s)):console.error("Invalid Arguments - ln-42")}function hideErrMsg(s){s?(s.classList.add("opacity-0"),s.classList.remove("opacity-100")):console.error("hideErrMsg:Invalid Arguments")}
+//# sourceMappingURL=index.b09637e9.js.map
